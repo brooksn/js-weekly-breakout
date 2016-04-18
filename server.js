fd07efceac5a8ed7541201a32584dec217abfa44
Brooks Newberry <mail@brooks.is>
2016-04-18 15:22:28 -0700

    change server response

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/rss+xml');
   updateFeed().then(function(xml){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/rss+xml');
     res.end(xml);
   })
-  res.end('Hello World\n');
+  //res.end('Hello World\n');
 })
 
 server.listen(port, hostname, () => {
