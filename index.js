60e8c8719a167df246e0c3e881f1e0d619c79ecf
Brooks Newberry <mail@brooks.is>
2016-04-18 13:45:07 -0700

    init

---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict';
+require('dotenv').config({silent: true});
+var fs = require('fs');
+var fetch = require('node-fetch')
+var RSS = require('rss');
+var cheerio = require('cheerio');
+
+const bucket = process.env.SMARTPODCASTER_S3_BUCKET;
+const awsid = process.env.SMARTPODCASTER_AWS_ID;
+const awskey = process.env.SMARTPODCASTER_AWS_KEY;
+const rootfileurl = process.env.ROOTS3FILEURL;
+const feedurl = process.env.FEEDURL;
+if (!bucket || !awsid || !awskey || !feedurl) throw Error('Set SMARTPODCASTER_S3_BUCKET, SMARTPODCASTER_AWS_ID and SMARTPODCASTER_AWS_KEY.');
+
+var aws = require('aws-s3-promisified')({
+    accessKeyId: awsid,
+    secretAccessKey: awskey
+});
+
+var feed = new RSS({
+  title: 'JavaScript Weekly',
+  feed_url: feedurl,
+  site_url: 'http://javascriptweekly.com/'
+});
+
+var latesturl = 'http://javascriptweekly.com/latest';
+
+var articles = [];
+var jobs = [];
+var inBrief = [];
+
+const jqSelector = 'html > body > div.containerweb > main > div.issue-html > table > tbody > tr > td > table:nth-of-type(2) > tbody > tr:nth-of-type(2) > td > table.gowide > tbody > tr > td'
+const mainSelector = 'div.issue-html > table > tr > td > table:nth-of-type(2) > tr:nth-of-type(2) > td > table.gowide > tr > td';
+const jobsSelector = 'div.issue-html > table > tr > td > table:nth-of-type(2) > tr:nth-of-type(2) > td > ul:nth-of-type(1) > li';
+const briefSelector = 'div.issue-html > table > tr > td > table:nth-of-type(2) > tr:nth-of-type(2) > td > ul:nth-of-type(2) > li';
+
+fetch(latesturl).then(function(res){
+  return res.text()
+}).then(function(text){
+  var $ = cheerio.load(text);
+  
+  $(mainSelector).each(function(index, td){
+    let title = $('div',td).eq(0).text();
+    let href = $('a',td).eq(0).attr('href');
+    let desc = $('div',td).eq(1).text();
+    let pub = $('div',td).eq(2).text();
+    let combo = title + href;
+    let guid = combo.replace(/\W/gi, "");
+    articles.push({title: title, href: href, description: desc, publisher: pub});
+    feed.item({
+      title: title,
+      description: desc,
+      url: href,
+      guid: guid,
+      author: pub
+    });
+  });
+  
+  $(jobsSelector).each(function(index, li){
+    let title = $('a',li).eq(0).text();
+    let href = $('a',li).eq(0).attr('href');
+    let desc = $('span',li).first().text();
+    let pub = $('span',li).last().text();
+    let combo = title + href;
+    let guid = combo.replace(/\W/gi, "");
+    jobs.push({title: title, href: href, description: desc, publisher: pub});
+    feed.item({
+      title: title,
+      description: desc,
+      url: href,
+      guid: guid,
+      author: pub
+    });
+  });
+  
+  $(briefSelector).each(function(index, li){
+    const spanCount = $('div',li).length;
+    let title = $('a',li).eq(0).text();
+    let href = $('a',li).eq(0).attr('href');
+    let tag = $('span',li).first().text();
+    let pub = $('span',li).last().text();
+    if (spanCount > 2) {
+      var desc = $('a',li).eq(1).text();
+    }
+    let combo = title + href;
+    let guid = combo.replace(/\W/gi, "");
+    inBrief.push({title: title, href: href, description: desc, publisher: pub, tag: tag});
+    feed.item({
+      title: title,
+      description: desc,
+      url: href,
+      guid: guid,
+      author: pub,
+      categories: [ tag ]
+    });
+  });
+  
+  console.log(articles[0]);
+  console.log(inBrief[0]);
+  console.log(jobs[0]);
+  var xml = feed.xml();
+  fs.writeFileSync('feed.xml', xml);
+  fs.writeFileSync(__dirname + '/feed.xml', xml);
+  aws.putFile(bucket, 'jsweekly/feed.xml', __dirname + '/feed.xml');
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+A node.js script to scrape javascriptweekly.com and save a more granular RSS feed.
+
+[Icon](https://thenounproject.com/icon54app/collection/video-games-line-icon/?i=236990) by by icon 54.
